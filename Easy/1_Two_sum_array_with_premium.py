c636e32404736a26a0dd688f8ef0e9469f20d873
Amit <amit5954@gmail.com>
2021-10-09 18:23:21 +0800

    First Accepted Program

--- Easy/1_Two_sum_array_with_premium.py
+++ Easy/1_Two_sum_array_with_premium.py
@@ -1,27 +1,15 @@
 class Solution:
     def twoSum(self, nums, target):
-        print("List is " + str(nums))
-        print("Target is " + str(target))
-        result = []
+        map = {}
         for i in range(len(nums)):
-            for j in range(len(nums)):
-                if i == j:
-                    j = j + 1
-                print("Sum is " + str(nums[i] + nums[j]))
-                if nums[i] + nums[j] == target:
-                    result.append(i)
-                    result.append(j)
-                    print("Result is " + str(result))
-                    return result
-                elif j < len(nums):
-                    j = j + 1
+            a = list(map.values())
+            current = nums[i]
+            x = target - current
+            if x in a:
+                print([a.index(x), i])
+                return [a.index(x), i]
+            map[i] = nums[i]
 
 
 a = Solution()
 a.twoSum([2, 11, 15, 7], 9)
-
-# This problem was solved by me , works ok on small list but if run on a huge list -
-# it time out - its time complexity is O(n2)
-
-
-
